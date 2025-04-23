f85a03fde5b983dd8cc2cbcc5a5d46a6dc4a5af7
Khalid <khalidm449@gmail.com>
2025-04-23 11:19:30 -0500

    man

--- backend/main.py
+++ backend/main.py
@@ -8,6 +8,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import hashlib
 import requests
+import os
 
 
 app = Flask(__name__)
@@ -103,5 +104,6 @@ def home():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
 
